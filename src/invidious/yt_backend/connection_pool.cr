67236e40e5c2d49f6b600b8137f54087405f7f31
mk-pmb <mk-pmb@users.noreply.github.com>
2024-01-02 23:40:15 +0100

    [⚠ DO NOT MERGE ⚠] Add whitespace to exactly match 9a5a743b.

--- src/invidious/yt_backend/connection_pool.cr
+++ src/invidious/yt_backend/connection_pool.cr
@@ -29,11 +29,13 @@ struct YoutubeConnectionPool
     if !CONFIG.http_proxy && region
       conn = make_client(url, region)
       conn.proxy = make_configured_http_proxy_client() if CONFIG.http_proxy
+
       response = yield conn
     else
       # Proxy needs to be reinstated every time we get a client from the pool
       conn = pool.checkout
       conn.proxy = make_configured_http_proxy_client() if CONFIG.http_proxy
+
       begin
         response = yield conn
       rescue ex
